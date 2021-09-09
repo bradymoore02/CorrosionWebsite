a651acd0032f4d5f4dc991f8b593e5015a381884
brady <bradyoore02@outlook.com>
2021-09-09 15:14:40 -0500

    Added 3d profilometry description

--- Corrosion/plotter/models.py
+++ Corrosion/plotter/models.py
@@ -5,7 +5,7 @@ import os
 
 def drop_upload_dir(instance, filename):
     return 'images/drops/{}/{}'.format(instance.drop.id, filename)
-    
+
 def sample_upload_dir(instance, filename):
     return 'images/samples/{}/{}'.format(instance.sample.id, filename)
 
@@ -13,22 +13,19 @@ def technique_upload_dir(instance, filename):
     return 'images/techniques/{}/{}'.format(instance.technique.id, filename)
 
 
-class Substrate(models.Model):
-    name = models.CharField(max_length=100)
-    code = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
 class Technique(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     detailed_description = models.TextField()
     def get_absolute_url(self):
         return reverse('technique-detail', args=[str(self.id)])
+    def __str__(self):
+        return self.name
 
 class MaterialType(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 
 class Material(models.Model):
@@ -40,6 +37,8 @@ class Material(models.Model):
     yield_strength = models.CharField(max_length=100)
     def get_absolute_url(self):
         return reverse('material-detail', args=[str(self.id)])
+    def __str__(self):
+        return self.name
 
 
 class Sample(models.Model):
@@ -48,6 +47,8 @@ class Sample(models.Model):
     roughness = models.CharField(max_length=100)
     mass_before = models.CharField(max_length=100)
     mass_after = models.CharField(max_length=100)
+    def __str__(self):
+        return "Sample " + str(self.number)
 
     def get_absolute_url(self):
         return reverse('sample-detail', args=[str(self.id)])
@@ -56,14 +57,11 @@ class Sample(models.Model):
 class SampleImage(models.Model):
     sample = models.ForeignKey(Sample, on_delete=models.CASCADE)
     image = models.ImageField(upload_to=sample_upload_dir)
+    def __str__(self):
+        return self.sample.name
 
 class TechniqueImage(models.Model):
     technique = models.ForeignKey(Technique, on_delete=models.CASCADE)
     image = models.ImageField(upload_to=technique_upload_dir)
-
-class Test(models.Model):
-    date = models.DateTimeField()
-    substrate = models.ForeignKey(Substrate, on_delete=models.PROTECT)
-
     def __str__(self):
-        return str(self.date.strftime('%m/%d/%Y'))
+        return self.technique.name
